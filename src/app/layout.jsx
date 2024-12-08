9850e8b599e98871abfd210d62cc75092a180d25
hritxx <hriteekroy1869@gmail.com>
2024-12-08 21:07:21 +0530

    fix-seo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,40 +1,9 @@
-// "use client";
-// import RootLayout from "@/components/RootLayout";
-// import { Analytics } from "@vercel/analytics/react";
-// import { SessionProvider } from "next-auth/react";
-
-// import "./globals.css";
-
-// export default function Layout({ children }) {
-//   return (
-//     <html
-//       lang="en"
-//       suppressHydrationWarning={true}
-//       className="h-full bg-blue-900 text-base antialiased text-neutral-100"
-//     >
-//       <title>Ortus Finance - Business Portfolio</title>
-//       <body className="flex min-h-full flex-col">
-//         <RootLayout>
-//           <SessionProvider>{children}</SessionProvider>
-//         </RootLayout>
-//         <Analytics />
-//       </body>
-//     </html>
-//   );
-// }
+"use client";
 import RootLayout from "@/components/RootLayout";
 import { Analytics } from "@vercel/analytics/react";
 import { SessionProvider } from "next-auth/react";
 
 import "./globals.css";
-
-export const metadata = {
-  title: "Ortus Finance - Empowering Business Growth",
-  description:
-    "Ortus Finance provides tailored financial solutions to help businesses thrive. Discover our portfolio and learn how we can accelerate your growth.",
-  url: "https://www.ortusfinance.in",
-  image: "https://www.ortusfinance.com/og-image.jpg",
-};
 
 export default function Layout({ children }) {
   return (
@@ -45,7 +14,10 @@ export default function Layout({ children }) {
     >
       <title>Ortus Finance - Business Portfolio</title>
       <body className="flex min-h-full flex-col">
-        <RootLayout>{children}</RootLayout>
+        <RootLayout>
+          <SessionProvider>{children}</SessionProvider>
+        </RootLayout>
+        <Analytics />
       </body>
     </html>
   );
